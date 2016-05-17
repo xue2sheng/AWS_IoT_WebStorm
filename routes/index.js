f599dc03c4bf27629f12dcfb0994465aece35d6b
Andres Sevillano <andres.sevillano@tecnotree.com>
2016-05-17 17:28:11 +0100

    Added github info

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Learning Cells AWS Shadow', welcome: "Simple Example for AWS IoT" });
+  res.render('index', {
+    title: 'Learning Cells AWS Shadow',
+    welcome: "Simple Example for AWS IoT",
+    github: "https://github.com/xue2sheng/AWS_IoT_WebStorm"
+  });
 });
 
 module.exports = router;
